626d28a0b1de966010477ed818700d270194e0de
Seth Fitzsimmons <seth@mojodna.net>
2008-11-25 20:29:26 -0800

    resource controller + views for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
-class PostsController < ApplicationController
+class PostsController < ResourceController::Base
+  skip_before_filter :verify_authenticity_token
+
+  create.wants.xml { render :xml => @post.to_xml  }
+  index.wants.xml  { render :xml => @posts.to_xml }
+  show.wants.xml   { render :xml => @post.to_xml  }
 end
--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,8 +1,57 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+require 'posts_controller'
 
-class PostsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+# Re-raise errors caught by the controller.
+class PostsController; def rescue_action(e) raise e end; end
+
+class PostsControllerTest < Test::Unit::TestCase
+  fixtures :posts
+
+  def setup
+    @controller = PostsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  def test_should_get_index
+    get :index
+    assert_response :success
+    assert assigns(:posts)
+  end
+
+  def test_should_get_new
+    get :new
+    assert_response :success
+  end
+  
+  def test_should_create_post
+    old_count = Post.count
+    post :create, :post => { }
+    assert_equal old_count+1, Post.count
+    
+    assert_redirected_to post_path(assigns(:post))
+  end
+
+  def test_should_show_post
+    get :show, :id => 1
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get :edit, :id => 1
+    assert_response :success
+  end
+  
+  def test_should_update_post
+    put :update, :id => 1, :post => { }
+    assert_redirected_to post_path(assigns(:post))
+  end
+  
+  def test_should_destroy_post
+    old_count = Post.count
+    delete :destroy, :id => 1
+    assert_equal old_count-1, Post.count
+    
+    assert_redirected_to posts_path
   end
 end
